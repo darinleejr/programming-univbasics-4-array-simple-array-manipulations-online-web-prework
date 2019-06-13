41fee6378c71eb88c4c4f0ad071a3264d148996b
Darin Webster <darinleenavy@gmail.com>
2019-06-13 21:51:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,7 @@ using_concat(array1, array2, new_array item0, item1, item2, item3, item4, item5)
 array1 = [item0, item1, item2]
 array2 = [item3, item4, item5]
 new_array = array1.concat(array2)
+new_array = [item0, item1, item2, item3, item4, item5]
 
 using_insert(array, element0, element1, element2, element3, new_element, string)
 array = [element1, element2, element3]
@@ -35,7 +36,7 @@ new_array = [item0, item1, item2, item3]
 
 using_flatten(array, flat_array, string0 string1, string2, string3)
 array = [string0, [string1, string2], string3]
-flat_array = arrary.flatten
+flat_array = array.flatten
 flat_array = [string0, string1, string2, string3]
 
 using_delete(array, string0, string1, string2, string3)
@@ -46,3 +47,4 @@ new_array = [string0, string1, string3]
 using_delete_at(array, element0, element1, element2)
 array = [element0, element1, element2]
 new_array = array.delete_at(1)
+new_array = [element0, element2]
