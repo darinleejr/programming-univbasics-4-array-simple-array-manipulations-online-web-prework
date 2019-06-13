88890addeaaeca471db55a062fcf86933b6972f7
Darin Webster <darinleenavy@gmail.com>
2019-06-13 20:52:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,12 +18,27 @@ item0 = array.using_shift
 shift_with_args(array, item0, item1, item2)
 {item1}, {item2} = array.shift
 
-using_concat(array1, array2, array3 item0, item1, item2, item3, item4, item5)
+using_concat(array1, array2, new_array item0, item1, item2, item3, item4, item5)
 array1 = [item0, item1, item2]
 array2 = [item3, item4, item5]
-array3 = array1.concat(array2)
+new_array = array1.concat(array2)
 
-using_insert(array1, array2, )
+using_insert(array, element0, element1, element2, element3, new_element, string)
+array = [element1, element2, element3]
+new_element[4] = string
+=> string
+
+using_uniq
+
+
+using_flatten
+
+
+using_delete
+
+
+using_delete_at
+
 
 
 
