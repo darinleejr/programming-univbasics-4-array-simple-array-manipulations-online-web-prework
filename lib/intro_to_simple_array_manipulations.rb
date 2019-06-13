f3ba351a4e624e593cfd2577069a7c2e95a43c05
Darin Webster <darinleenavy@gmail.com>
2019-06-13 19:51:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,10 @@ using_push(array, element)
 using_push = element
 array.push(using_push)
 
+using_unshift(array, element)
+using_unshift = element
+array.unshift(using_unshift)
 
-pets.unshift("element")
+using_pop(array, element)
+array.pop
 
