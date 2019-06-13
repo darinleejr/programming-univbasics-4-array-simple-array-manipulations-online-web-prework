020e91dd429473e7125ffe771001ac365433813f
Darin Webster <darinleenavy@gmail.com>
2019-06-13 20:21:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,15 @@ using_pop(array, element0, element1, element2)
 element2 = array.pop
 
 pop_with_args(array, item0, item1, item2)
-
+{item1}, {item2} = array.pop
 
 using_shift(array, item0, item1, item2)
+item0 = array.using_shift
+
+shift_with_args(array, item0, item1, item2)
+{item1}, {item2} = array.shift
+
+
+
+
+
