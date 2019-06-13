e904a020d257a9f9235ae4ae97d1762ca5fc62cc
Darin Webster <darinleenavy@gmail.com>
2019-06-13 20:08:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,11 +1,12 @@
-using_push(array, element)
-using_push = element
+using_push(array, string)
+using_push = string
 array.push(using_push)
 
-using_unshift(array, element)
-using_unshift = element
+using_unshift(array, string)
+using_unshift = string
 array.unshift(using_unshift)
 
-using_pop(array, string)
-string = array.pop
+using_pop(array, element0, element1, element2)
+element2 = array.pop
 
+pop_with_args(array, item0, item1, item2)
